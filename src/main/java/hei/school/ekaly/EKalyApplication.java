16af65db8b5aaad116c7bd696185f93d8619d147
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2024-05-27 18:42:38 +0300

    chore: generate an empty gradle kotlin project

---
+++ src/main/java/hei/school/ekaly/EKalyApplication.java
@@ -0,0 +1,13 @@
+package hei.school.ekaly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKalyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EKalyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hei/school/ekaly/EKalyApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.ekaly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EKalyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
